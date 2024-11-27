3ac169e6876bd9367e7f5839c025be46de7447a9
Priyanshu Singh <priyanshu@Priyanshus-MacBook-Air.local>
2024-11-27 20:40:13 +0530

    React 1 Project

---
+++ React Practice/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Header'
+import MainContent from './MainContent'
+
+function Page() {
+  return(
+    <>
+    <Header />
+    <MainContent />
+    </>
+  )
+}
+
+export default Page
---
+++ React Practice/src/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <header className="header">
+            <img className="header-react-icon" src="/src/assets/react.svg" alt="" />
+            <h1 className="header-heading">ReactFacts</h1>
+        </header>
+    )
+}
