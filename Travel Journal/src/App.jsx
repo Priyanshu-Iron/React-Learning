e596a0b5781c76109c02bbaf0cac35da1ebdacff
Priyanshu Singh <priyanshu@Priyanshus-MacBook-Air.local>
2024-11-29 12:13:55 +0530

    Header Added

---
+++ Travel Journal/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from './components/Header'
+
+function Page() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default Page
---
+++ Travel Journal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return(
+        <header>
+            <img className="gloab-icon" src="/src/assets/globe.svg" alt="gloab-icon" />
+            <h2 className="heading">my travel journal.</h2>
+        </header>
+    )
+}
