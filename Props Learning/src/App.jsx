49e049559750e5e0952b4f99047fa654c8fe47a1
Priyanshu Singh <priyanshu@Priyanshus-MacBook-Air.local>
2024-11-29 20:16:30 +0530

    Learning Prop

---
+++ Props Learning/src/App.jsx
@@ -0,0 +1,34 @@
+import Contact from './Contact'
+
+function App() {
+  return(
+    <>
+    <Contact 
+      img = "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg"
+      name = "Mr. Whiskerson"
+      phone = "(212) 555-1234"
+      email = "mr.whiskaz@catnap.meow"
+    />
+    <Contact 
+      img = "https://plus.unsplash.com/premium_photo-1673967831980-1d377baaded2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2F0c3xlbnwwfHwwfHx8MA%3D%3D"
+      name = "Fluffykins"
+      phone = "(212) 555-2345"
+      email = "fluff@me.com"
+    />
+    <Contact 
+      img = "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg"
+      name = "Felix"
+      phone = "(212) 555-4567"
+      email = "thecat@hotmail.com"
+    />
+    <Contact 
+      img = "https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_640.jpg"
+      name = "Pumpkin"
+      phone = "(0800) CAT KING"
+      email = "pumpkin@scrimba.com"
+    />
+    </>
+  )
+}
+
+export default App
